883ba8e57915125adda308bec0c3befcf9e41555
Hassan-ELMAKHLOUFI <hassanelmakhloufi2000@gmail.com>
2022-03-30 23:37:10 +0000

    Create SpringLdapApplicationTests.java

---
+++ src/test/java/hassan/ma/springldap/SpringLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package hassan.ma.springldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLdapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
