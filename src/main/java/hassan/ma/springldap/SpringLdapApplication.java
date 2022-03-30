4d72c7d71153243c489bb12494abb5b8502d698d
Hassan-ELMAKHLOUFI <hassanelmakhloufi2000@gmail.com>
2022-03-30 23:37:07 +0000

    Create SpringLdapApplication.java

---
+++ src/main/java/hassan/ma/springldap/SpringLdapApplication.java
@@ -0,0 +1,13 @@
+package hassan.ma.springldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLdapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLdapApplication.class, args);
+    }
+
+}
